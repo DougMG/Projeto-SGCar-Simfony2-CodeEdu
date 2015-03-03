002c22f4fd22cfd1b6054df096c9ce06adf8975e
Douglas Marques <dougmarques10@gmail.com>
2015-03-02 21:17:07 -0300

    - FASE 2 DO PROJETO: CRIANDO BUNDLE
        - Adicionando o Bundle: CodeCarBundle
        - Criando o Controller: CarController.php no CodeCarBundle
        - Criando a indexAction() do controller: CarController.php
        - Alterando o arquivo de rotas: routing.yml com rota cars para o bundle CodeCarBundle/controller: CarController
        - Criando uma Lista de automóveis por marca e modelos na view index.html.twig do controller: CarController.php do Bundle: CodeCarBundle

---
+++ src/Code/CarBundle/Controller/CarController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Code\CarBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CarController extends Controller
+{
+    /**
+     * @Route(name="index.html.twig")
+     * @Template("CodeCarBundle:Car:index.html.twig")
+     */
+    public function indexAction()
+    {
+        $automoveis = array('Fiat' => ["Palio", "Strada", "Linea", "Punto" ],
+                                    'Wolksvagen' => ["Gol", "Fox", "Saveiro", "Amarock"],
+                                    'Honda' => ["Civic", "New Civic"],
+                                    'Chevrolet' => ["Camaro", "S10", "Onix", "Cruzer"]);
+
+        $resultado = array();
+
+        foreach ($automoveis as  $key=>$automovel) {
+                //$result[$key][] = $automovel;
+            foreach ($automovel as $value) {
+                $resultado[$key][] = $value;
+            }
+        }
+
+        return $this->render('CodeCarBundle:Car:index.html.twig', ["resultados"=>$resultado]);
+    }
+}
