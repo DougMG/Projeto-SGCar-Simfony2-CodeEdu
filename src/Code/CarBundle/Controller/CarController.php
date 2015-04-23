c5e53b5f2ac3962af112d779b4b633fa15b6a026
Douglas Marques <dougmarques10@gmail.com>
2015-04-22 23:03:29 -0300

    - FASE 3 DO PROJETO: CRIANDO ENTIDADES
            - Criadas as Entidades: Carro, CarroRepository, Fabricante e FabricanteRepository  no Controller - CodeCarBundle
            - Criada a doctrineAction() no controller: CarController.php
            - Rota para a action doctrine: cars/doctrine
    	- Gerado o Dump e Criado o Schema de banco de dados para as Entidades Carro e Fabricante, com uso das annotations do Doctrine
            - Criada uma Lista de automóveis na view Car/doctrine.html.twig
    
    	- modificado: src/AppBundle/Resources/views/default/index.html.twig
    	- modificado: src/Code/CarBundle/Controller/CarController.php
    	- new file:   src/Code/CarBundle/Entity/Carro.php
    	- new file:   src/Code/CarBundle/Entity/CarroRepository.php
    	- new file:   src/Code/CarBundle/Entity/Fabricante.php
    	- new file:   src/Code/CarBundle/Entity/FabricanteRepository.php
    	- new file:   src/Code/CarBundle/Resources/views/Car/doctrine.html.twig

--- src/Code/CarBundle/Controller/CarController.php
+++ src/Code/CarBundle/Controller/CarController.php
@@ -6,6 +6,7 @@ namespace Code\CarBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Code\CarBundle\Entity\Carro;
 
 class CarController extends Controller
 {
@@ -13,6 +14,7 @@ class CarController extends Controller
      * @Route("/", name="index_cars")
      * @Template("CodeCarBundle:Car:index.html.twig")
      */
+    //2ª Etapa do Projeto - Gerenciamento de Carros
     public function indexAction()
     {
         $automoveis = array('Fiat' => ["Palio", "Strada", "Linea", "Punto" ],
@@ -30,5 +32,29 @@ class CarController extends Controller
         }
 
         return $this->render('CodeCarBundle:Car:index.html.twig', ["resultados"=>$resultado]);
+    }
+
+    // 3ª Etapa do Projeto - Gerenciamento de Carros
+    /**
+    * @Route("/doctrine", name="doctrine_cars")
+    * @Template("CodeCarBundle:Car:doctrine.html.twig")
+    */
+    public function doctrineAction()
+    {
+        $carro = new Carro;
+        $carro->setModelo("Model 03");
+        $carro->setFabricante("Fabricante 03");
+        $carro->setAno(2004);
+        $carro->setCor("Branco");
+
+        $manager = $this->getDoctrine()->getEntityManager();
+        $manager->persist($carro);
+        $manager->flush();
+
+
+        $repository = $manager->getRepository("CodeCarBundle:Carro");
+        $carros = $repository->findAll();
+
+        return $this->render('CodeCarBundle:Car:doctrine.html.twig', ['carros'=>$carros]);
     }
 }
---
+++ src/Code/CarBundle/Entity/Carro.php
@@ -0,0 +1,86 @@
+<?php
+namespace Code\CarBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Table()
+* @ORM\Entity()
+*/
+class Carro{
+
+    /**
+    * @var integer
+    * @ORM\Column(name="id", type="integer", length=255)
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+    private $id;
+
+    /**
+    * @var string
+    * @ORM\Column(name="modelo", type="string", length=100)
+    */
+    private $modelo;
+
+    /**
+    * @var string
+    * @ORM\Column(name="fabricante", type="string", length=100)
+    */
+    private $fabricante;
+
+    /**
+    * @var integer
+    * @ORM\Column(name="ano", type="integer", length=15)
+    */
+    private $ano;
+
+    /**
+    * @var string
+    * @ORM\Column(name="cor", type="string", length=100)
+    */
+    private $cor;
+
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getModelo(){
+        return $this->modelo;
+    }
+
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+
+    public function getFabricante(){
+        return $this->fabricante;
+    }
+
+    public function setFabricante($fabricante){
+        $this->fabricante = $fabricante;
+    }
+
+    public function getAno(){
+        return $this->ano;
+    }
+
+    public function setAno($ano){
+        $this->ano = $ano;
+    }
+
+    public function getCor(){
+        return $this->cor;
+    }
+
+    public function setCor($cor){
+        $this->cor = $cor;
+    }
+
+}
+?>
